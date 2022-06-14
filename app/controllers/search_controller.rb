d0f30f8de43afdce136c5842992e3108e405d8ef
OrGranot <orgranot91@gmail.com>
2022-06-14 12:45:32 +0200

    api rank search complete

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,12 +5,39 @@ class SearchController < ApplicationController
     @keywords.each do |keyword|
       search = keyword.searches.select {|s| s[:date] == Date.today}
       unless search != []
-        search = Search.new(date: Date.today, rank: rand(1..50), engine: 'Google')
-        search.keyword = keyword
-        search.save
+        rank = find_rank(keyword)
+        if rank > 0 then
+          search = Search.new(date: Date.today, rank: rank, engine: 'Google')
+          search.keyword = keyword
+          search.save
+        end
       end
-
     end
     redirect_to website
   end
+
+
+  private 
+  
+  def find_rank(keyword)
+    results = api_call(keyword)
+    results["organic_results"].each do |result|
+      if result["domain"] == (keyword.website.url) then return result["position"] end
+    end
+    return -1
+  end
+
+  def api_call(query, gl = "il")
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    uri = URI("https://api.valueserp.com/search")
+
+    params = { :api_key => "5996D87454614FAA8AC47EF89787C904", :q => query.key_string, :gl => gl, :num => 100 }
+    uri.query = URI.encode_www_form(params)
+
+    res = Net::HTTP.get_response(uri)
+    results = JSON.parse(res.read_body)
+  end 
 end
