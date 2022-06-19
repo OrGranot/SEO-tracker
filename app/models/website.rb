bc1cebcddf4cf7be4896b3926a489e0e8fa8c77e
OrGranot <orgranot91@gmail.com>
2022-06-19 14:43:04 +0200

    validations added

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,4 +2,7 @@ class Website < ApplicationRecord
   belongs_to :user
   has_many :keywords, dependent: :destroy
   has_many :searches, through: :keywords
+  validates :name, presence: true
+  validates :url, presence: true
+
 end
