ccd63b5158f1759b305a9462c6f99209bb24112b
Or Granot <orgranot91@gmail.com>
2022-06-24 11:15:05 +0200

    user website shares table droped

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_110452) do
+ActiveRecord::Schema.define(version: 2022_06_24_091004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,15 +32,6 @@ ActiveRecord::Schema.define(version: 2022_06_23_110452) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["keyword_id"], name: "index_searches_on_keyword_id"
-  end
-
-  create_table "user_website_shares", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "website_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_user_website_shares_on_user_id"
-    t.index ["website_id"], name: "index_user_website_shares_on_website_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,7 +57,5 @@ ActiveRecord::Schema.define(version: 2022_06_23_110452) do
 
   add_foreign_key "keywords", "websites"
   add_foreign_key "searches", "keywords"
-  add_foreign_key "user_website_shares", "users"
-  add_foreign_key "user_website_shares", "websites"
   add_foreign_key "websites", "users"
 end
