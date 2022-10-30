96fd0233854830dd135992163f184e69925c92a5
Or Granot <orgranot91@gmail.com>
2022-10-30 21:08:51 +0100

    add web button in sidebar

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,4 +2,5 @@ class Keyword < ApplicationRecord
   belongs_to :website
   has_many :searches, dependent: :destroy
   validates :key_string, presence: true
+  validates :key_string, uniqueness: true
 end
