1e40e8b48b28c6473e3ab4e2a56645d62e98a4f5
Or Granot <orgranot91@gmail.com>
2022-11-04 14:55:24 +0100

    chartkick

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,16 @@
+Chartkick.options = {
+  height: "50vh",
+  empty: "לא קיים מידע",
+  loading: "טוען מידע",
+  xtitle: "תאריך", ytitle: "מיקום",
+
+  min: 1, max: 100,
+  library: {
+    animation: {easing: 'easeOutQuart'},
+    scales: {
+      y: {
+        reverse: true
+      }
+    }
+  }
+}
