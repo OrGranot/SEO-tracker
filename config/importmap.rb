1510aa9d737d6d67a74879b1738a5a6f94ddd967
Or Granot <orgranot91@gmail.com>
2022-11-07 11:18:24 +0100

    checkout

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.2.0/js/all.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
