d7cf7565a4ba7b602f019ae66f13ff633b17d329
Or Granot <orgranot91@gmail.com>
2022-11-09 14:03:35 +0100

    added uniqeness to website name and url

--- app/models/website.rb
+++ app/models/website.rb
@@ -5,7 +5,9 @@ class Website < ApplicationRecord
   has_many :users, through: :shared_websites
   has_many :shared_websites, dependent: :destroy
   validates :name, presence: true
+  validates :name, uniqueness: true
   validates :url, presence: true
+  validates :url, uniqueness: true
 
   def group_web_data
     self.keywords.map { |keyword| keyword.searches }.flatten.group_by { |s| s.date }
