8857609dbadecd6f4fb0f8628a16efc83c845817
Or Granot <orgranot91@gmail.com>
2022-11-13 11:42:15 +0100

    chartkick is smaller

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,10 +1,10 @@
 Chartkick.options = {
-  height: "50vh",
+  height: "30vh",
   empty: "לא קיים מידע",
   loading: "טוען מידע",
   xtitle: "תאריך", ytitle: "מיקום",
 
-  min: 1, max: 100,
+  min: 1, max: 50,
   library: {
     animation: {easing: 'easeOutQuart'},
     scales: {
